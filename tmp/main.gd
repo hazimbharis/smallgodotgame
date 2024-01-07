687655a3d033a5bef11ca916ed3b37b0db855bc1
Zim <98758788+hazimbharis@users.noreply.github.com>
2024-01-07 08:26:54 +0000

    made movement better
    bit unoptimized probably

--- tmp/main.gd
+++ tmp/main.gd
@@ -2,11 +2,18 @@ extends Node3D
 @onready var camera = $CharacterBody3D/Camera3D
 @onready var player = $CharacterBody3D
 var rotating = false
+var left = 1;
+var down = 2;
+var right = 3;
+var up = 4;
+
+# when changing camera, just shift ui keys left-wise or right-wise according to rotation amounts
+# [l,d,r,u]
 
 func _ready():
 	player.transform.origin
 
-func _input(event: InputEvent) -> void:
+func _input(event: InputEvent) -> void:	
 	if event.is_action_released("ui_text_caret_word_left"):
 		rotating = false
 	if !event.is_pressed():
@@ -16,20 +23,80 @@ func _input(event: InputEvent) -> void:
 	
 	if rotating:
 		print(player.rotation)
-		if event.is_action("ui_up"):
+		if event.is_action("camera_up"):
 			player.rotation.y += 0
-		elif event.is_action("ui_right"):
+		elif event.is_action("camera_right"):
 			player.rotation.y -= deg_to_rad(90)
-		elif event.is_action("ui_down"):
+			left -= 1;
+			if left == 0: left = 4;
+			down -= 1;
+			if down == 0: down = 4;
+			right -= 1;
+			if right == 0: right = 4;
+			up -= 1;
+			if up == 0: up = 4;
+		elif event.is_action("camera_down"):
 			player.rotation.y += deg_to_rad(180)
-		elif event.is_action("ui_left"):
+			left += 2;
+			if left == 5: left = 1;
+			if left == 6: left = 2;
+			down += 2;
+			if down == 5: down = 1;
+			if down == 6: down = 2;
+			right += 2;
+			if right == 5: right = 1;
+			if right == 6: right = 2;
+			up += 2;
+			if up == 5: up = 1;
+			if up == 6: up = 2;
+		elif event.is_action("camera_left"):
 			player.rotation.y += deg_to_rad(90)
+			left += 1;
+			if left == 5: left = 1;
+			down += 1;
+			if down == 5: down = 1;
+			right += 1;
+			if right == 5: right = 1;
+			up += 1;
+			if up == 5: up = 1;
 	else:
-		if event.is_action("ui_left"):
-			player.position.x -= 2
-		elif event.is_action("ui_right"):
-			player.position.x += 2
-		elif event.is_action("ui_up"):
-			player.position.z -= 2
-		elif event.is_action("ui_down"):
-			player.position.z += 2
+		if event.is_action("move_left"):
+			match left:
+				1:
+					player.position.x -= 2;
+				2:
+					player.position.z += 2;
+				3:
+					player.position.x += 2;
+				4:
+					player.position.z -= 2;
+		elif event.is_action("move_right"):
+			match right:
+				1:
+					player.position.x -= 2;
+				2:
+					player.position.z += 2;
+				3:
+					player.position.x += 2;
+				4:
+					player.position.z -= 2;
+		elif event.is_action("move_up"):
+			match up:
+				1:
+					player.position.x -= 2;
+				2:
+					player.position.z += 2;
+				3:
+					player.position.x += 2;
+				4:
+					player.position.z -= 2;
+		elif event.is_action("move_down"):
+			match down:
+				1:
+					player.position.x -= 2;
+				2:
+					player.position.z += 2;
+				3:
+					player.position.x += 2;
+				4:
+					player.position.z -= 2;

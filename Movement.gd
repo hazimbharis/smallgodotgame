e8ed6248d8d52ec4da46edc51ab34765b054d851
Melonial <mellowfjord@gmail.com>
2024-01-10 19:53:36 +0000

    Implemented movement and rotation via basis, achieving 'Tank Control'

--- Movement.gd
+++ Movement.gd
@@ -3,7 +3,7 @@ var player
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	player = get_node("CharacterBody3D")
+	player = get_node("Llama")
 	player.basis.x = Vector3(1,0,0)
 	player.basis.y = Vector3(0,1,0)
 	player.basis.z = Vector3(0,0,1)
@@ -13,11 +13,14 @@ func _process(delta):
 	pass
 
 func _input(event: InputEvent) -> void:	
-	if event.is_action_pressed("move_up"):
+	if event.is_action_pressed("ui_right"):
 		player.position += -2*player.basis.z
-	if event.is_action_pressed("move_down"):
+	if event.is_action_pressed("ui_left"):
 		player.position += 2*player.basis.z
-	if event.is_action_pressed("move_left"):
+	if event.is_action_pressed("ui_up"):
 		player.position += -2*player.basis.x
-	if event.is_action_pressed("move_right"):
+	if event.is_action_pressed("ui_down"):
 		player.position += 2*player.basis.x
+	if event.is_action_pressed("Rotate"):
+		player.basis = player.basis.rotated(Vector3(0,1,0),PI/2)
+		#print( player.basis.rotated(Vector3(0,1,0),PI) )
